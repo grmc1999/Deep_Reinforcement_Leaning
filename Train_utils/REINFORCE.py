1d38612d184508237e9bbfabbe9dffd3d69b233d
guillermo <gmorenoc@uni.pe>
2022-08-17 17:25:18 -0500

    inits

--- Train_utils/REINFORCE.py
+++ Train_utils/REINFORCE.py
@@ -41,10 +41,10 @@ class REINFORCE(object):
 
     def run_episode(self):
         u=self.free_input
-        s, reward, done, _=self.env.reset()
+        s=self.env.reset()
         self.episodes_states[self.current_episode]=[s]
         self.episodes_action[self.current_episode]=[u]
-        self.episodes_rewards[self.current_episode]=[reward]
+        self.episodes_rewards[self.current_episode]=[0]#consider size of rewards equal to 1 less than action and states
 
 
         #Generate episode
